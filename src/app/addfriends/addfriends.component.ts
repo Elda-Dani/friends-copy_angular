1adc286f46dcebe33635974d96b3b868e4b142c5
Elda-Dani <eldadani2001@gmail.com>
2022-10-14 20:49:24 +0530

    app

---
+++ src/app/addfriends/addfriends.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addfriends',
+  templateUrl: './addfriends.component.html',
+  styleUrls: ['./addfriends.component.css']
+})
+export class AddfriendsComponent implements OnInit {
+
+  constructor() { }
+
+
+  
+  ngOnInit(): void {
+  }
+
+}
